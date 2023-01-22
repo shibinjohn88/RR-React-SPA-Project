2543ed19f7d3823b0d29774f8abb31e692da38d1
shibinjohn88 <shibinjohnpdlm@gmail.com>
2023-01-22 17:29:23 -0500

    tmdb api fetched using axios for dynamic banner

--- moviedb/src/App.js
+++ moviedb/src/App.js
@@ -2,15 +2,35 @@ import './App.css';
 import Banner from './Components/Banner/Banner';
 import MovieCard from './Components/MovieCard/MovieCard';
 import Navbar from './Components/Navbar/Navbar';
+import axios from 'axios'
+import { useState } from 'react';
 
 function App() {
+  const [state,setState] = useState('')
   return (
     <div className="App">
       <Navbar/>
-      <Banner/>
+      <Banner state={state} />
       <MovieCard/>
+      <button onClick={() => {
+        // axios.get('https://jsonplaceholder.typicode.com/posts').then((response)=>{
+        //   console.log(response.data)
+        //   setState(response.data)
+        // })
+        setState("load")
+      }}>Click me</button>
+      {/* {state.map((obj, index) => {
+        return(
+          <div>
+            <h1>{index}</h1>
+            <h4>{obj.title}</h4>
+          </div>
+        )
+      })
+      } */}
     </div>
   );
 }
 
 export default App;
+    
---
+++ moviedb/src/constants/constants.js
@@ -0,0 +1,4 @@
+export const baseUrl = 'https://api.themoviedb.org/3'
+export const API_KEY = '2186c8fcda107afc8d4e5f502d9ebd25'
+export const imageUrl = 'https://image.tmdb.org/t/p/original'
+
